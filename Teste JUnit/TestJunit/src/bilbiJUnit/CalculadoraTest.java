b9b14c7c089eebda58030dd11f7f06e80dcff318
julianohe <104738855+julianohe@users.noreply.github.com>
2023-07-10 06:46:00 -0300

    Testes c/ JUnit

---
+++ Teste JUnit/TestJunit/src/bilbiJUnit/CalculadoraTest.java
@@ -0,0 +1,18 @@
+package bilbiJUnit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculadoraTest {
+    private Assertions Assert;
+
+    @Test
+        public void deveriaSomarDoisNumerosPositivos() {
+            Calculadora calc = new Calculadora();
+            int soma = calc.somar(3, 7);
+
+            Assertions.assertEquals(10, soma);
+        }
+
+
+}
