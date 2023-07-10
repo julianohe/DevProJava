3965fca285c83200ac3736e2fc6526c2b7fb8093
julianohe <104738855+julianohe@users.noreply.github.com>
2023-07-10 06:57:41 -0300

    Test c/ JUnit

---
+++ TestJunit/src/bibliJunit/Calculadora.java
@@ -0,0 +1,7 @@
+package bibliJunit;
+
+public class Calculadora {
+    public int somar (int a, int b) {
+        return a + b;
+    }
+}
---
+++ TestJunit/src/bibliJunit/CalculadoraTest.java
@@ -0,0 +1,13 @@
+package bibliJunit;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+public class CalculadoraTest {
+
+    private Assertions Assert;
+
+    @Test
+    public void deveriaSomarDoisNumerosPositivos() {
+        Calculadora calc = new Calculadora();
+        int soma = calc.somar(3, 7);
+    }
+}
